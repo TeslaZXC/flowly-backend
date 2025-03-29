f9774891b5c3a6f8bfcd785a5202c492a6d75acb
TeslaQ1 <104022375+TeslaZXC@users.noreply.github.com>
2025-03-29 04:42:28 +0300

    v3

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,16 +6,25 @@ export const register = async (req, res) => {
   const { username, password, personalInfo } = req.body;
 
   try {
+    const existingUser = await findUserByUsername(username);
+    if (existingUser.length > 0) {
+      return res.status(409).json({ message: 'Пользователь с таким логином уже существует' });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     createUser(username, hashedPassword, personalInfo, (err, result) => {
-      if (err) return res.status(500).json({ message: 'Ошибка регистрации' });
+      if (err) {
+        console.error('Registration Error: ', err);
+        return res.status(500).json({ message: 'Ошибка регистрации. Пожалуйста, попробуйте позже.' });
+      }
 
       const token = generateToken({ id: result.insertId, username, personal_info: personalInfo });
       res.status(201).json({ token });
     });
   } catch (error) {
-    res.status(500).json({ message: 'Ошибка сервера' });
+    console.error('Error in register: ', error);
+    res.status(400).json({ message: 'Ошибка сервера. Пожалуйста, попробуйте позже.' });
   }
 };
 
@@ -23,7 +32,12 @@ export const login = (req, res) => {
   const { username, password } = req.body;
 
   findUserByUsername(username, async (err, results) => {
-    if (err || results.length === 0) {
+    if (err) {
+      console.error('Login Error: ', err);
+      return res.status(500).json({ message: 'Ошибка сервера. Пожалуйста, попробуйте позже.' });
+    }
+
+    if (results.length === 0) {
       return res.status(400).json({ message: 'Неверный логин или пароль' });
     }
 
